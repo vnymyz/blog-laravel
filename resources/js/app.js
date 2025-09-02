5b33fbf44663c45947f579376c50eb7e256b98e9
DanYuki <daniyusuf.muhammad@gmail.com>
2025-09-02 20:44:53 +0700

    Add basic feature to show all posts with basic pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import 'flowbite'; // Tambahkan package flowbite untuk fungsi dropdown atau modal dari flowbite component
 
 window.Alpine = Alpine;
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
@@ -53,9 +54,7 @@ Route::get('/hello', function () {
     return 'hello semuanya';
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
