b6150ad8a155a219987bfefa65d15b9f8f399c9a
DanYuki <daniyusuf.muhammad@gmail.com>
2025-09-02 20:51:24 +0700

    Fixed action button dropdown toggle

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -71,30 +71,30 @@
                                         <tbody>
                                             @forelse ($posts as $post)
                                                 <tr class="border-b dark:border-gray-700">
-                                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $loop->iteration }}</th>
-                                                    <td scope="row"
-                                                        class="px-4 py-3">
+                                                    <th scope="row"
+                                                        class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $loop->iteration }}</th>
+                                                    <td scope="row" class="px-4 py-3">
                                                         {{ $post->title }}</td>
                                                     <td class="px-4 py-3">{{ $post->author->name }}</td>
                                                     <td class="px-4 py-3">{{ $post->category->name }}</td>
                                                     <td class="px-4 py-3 max-w-[12rem] truncate">
                                                         {{ $post->created_at->diffForHumans() }}</td>
                                                     <td class="px-4 py-3 flex items-center justify-end">
-                                                        <button id="apple-imac-27-dropdown-button"
-                                                            data-dropdown-toggle="apple-imac-27-dropdown"
+                                                        <button id="post-{{ $post->id }}-dropdown-button"
+                                                            data-dropdown-toggle="post-{{ $post->id }}-dropdown"
                                                             class="inline-flex items-center text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-700 p-1.5 dark:hover-bg-gray-800 text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                                                             type="button">
-                                                            <svg class="w-5 h-5" aria-hidden="true"
-                                                                fill="currentColor" viewbox="0 0 20 20"
-                                                                xmlns="http://www.w3.org/2000/svg">
+                                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                                viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                                 <path
                                                                     d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                             </svg>
                                                         </button>
-                                                        <div id="apple-imac-27-dropdown"
+                                                        <div id="post-{{ $post->id }}-dropdown"
                                                             class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                                                             <ul class="py-1 text-sm"
-                                                                aria-labelledby="apple-imac-27-dropdown-button">
+                                                                aria-labelledby="post-{{ $post->id }}-dropdown-button">
                                                                 <li>
                                                                     <button type="button"
                                                                         data-modal-target="updateProductModal"
